a7cdcea989dbc4db30a90ac114ae2da709792d13
hobbleabbas <shakeel@atlasfinance.org>
2022-10-19 17:48:04 -0400

    ngrok saves the day

--- pages/api/backend/donation/create/index.ts
+++ pages/api/backend/donation/create/index.ts
@@ -1,10 +1,9 @@
-import { SimpleMessageResponse } from "../../../../../systems/classes/interfaces";
 import create_donation from "../../../../../systems/functions/create_donation";
 
 export default async function handler(req, res) {
     try {
-
-        const { donation_id } = req.data.object.id
+        console.log(req)
+        const { donation_id } = req.body.id
         const donation = await create_donation(donation_id)
         res.status(200).send({ "status": 200, donation: donation });
 
