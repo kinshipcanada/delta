d170b3ae5b40d699a183b9fa692d6a9905fb08a2
Shakeel Hussein <shakeelhussein@Shakeels-MBP.ht.home>
2024-02-21 22:33:19 -0500

    fix stripe webhook issues

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -11,7 +11,7 @@ export default function Dashboard() {
 
     return (
         <div>
-            Dashboard, {donor?.first_name}
+            Dashboard, {donor!.donorFirstName}
         </div>
     )
 }
