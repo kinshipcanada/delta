9df0f035a5a7d2fbfca151978d468ab8b0499e18
Shakeel Hussein <shakeelhussein@Shakeels-MBP.ht.home>
2024-02-23 12:13:54 -0500

    remove logo cloud from campaign page{

--- pages/campaigns/ramadhan.jsx
+++ pages/campaigns/ramadhan.jsx
@@ -197,58 +197,6 @@ export default function Ramadhan() {
         {/* <Example /> */}
 
         {/* Logo cloud */}
-        <div className="relative isolate -z-10 mt-4 sm:mt-48">
-          <div className="absolute inset-x-0 top-1/2 -z-10 flex -translate-y-1/2 justify-center overflow-hidden [mask-image:radial-gradient(50%_45%_at_50%_55%,white,transparent)]">
-            <svg className="h-[40rem] w-[80rem] flex-none stroke-gray-200" aria-hidden="true">
-              <defs>
-                <pattern
-                  id="e9033f3e-f665-41a6-84ef-756f6778e6fe"
-                  width={200}
-                  height={200}
-                  x="50%"
-                  y="50%"
-                  patternUnits="userSpaceOnUse"
-                  patternTransform="translate(-100 0)"
-                >
-                  <path d="M.5 200V.5H200" fill="none" />
-                </pattern>
-              </defs>
-              <svg x="50%" y="50%" className="overflow-visible fill-gray-50">
-                <path d="M-300 0h201v201h-201Z M300 200h201v201h-201Z" strokeWidth={0} />
-              </svg>
-              <rect width="100%" height="100%" strokeWidth={0} fill="url(#e9033f3e-f665-41a6-84ef-756f6778e6fe)" />
-            </svg>
-          </div>
-          <div className="mx-auto max-w-7xl px-6 lg:px-8">
-            <h2 className="text-center text-lg font-semibold leading-8 text-gray-900">
-              Backed By Partners On The Ground
-            </h2>
-            <div className="mx-auto mt-10 grid max-w-lg grid-cols-3 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-3 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-              <img
-                className="col-span-2 max-h-24 w-full object-contain lg:col-span-1"
-                src="/partner-logos/desk-and-chair.png"
-                alt="Transistor"
-                width={158}
-                height={48}
-              />
-              <img
-                className="col-span-2 max-h-24 w-full object-contain lg:col-span-1"
-                src="/logo-full-size.png"
-                alt="Reform"
-                width={158}
-                height={48}
-              />
-              <img
-                className="col-span-2 max-h-24 w-full object-contain lg:col-span-1"
-                src="/partner-logos/al-imam.png"
-                alt="Tuple"
-                width={158}
-                height={48}
-              />
-            </div>
-          </div>
-        </div>
-
 
 
       </main>
