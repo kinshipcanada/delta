ceef3e0d0322b9dd1bb808cbda964277e3051f0b
hobbleabbas <shakeel.hussein@mail.utoronto.ca>
2024-02-26 14:00:48 -0500

    upload proof

--- pages/api/v2/donations/manual.ts
+++ pages/api/v2/donations/manual.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import * as Sentry from "@sentry/nextjs";
 import { DonationEngine } from "@lib/methods/donations";
 import { Donation } from "@prisma/client";
+import { NotificationEngine } from "@lib/methods/notifications";
 
 /**
  * @description Allows admin to create a new donation manually
@@ -12,10 +13,15 @@ export default async function handler(
 ) {
     try {
         const payload = req.body.donation as Donation
+        console.log("Received payload: ")
+        console.log(payload)
         const donationEngine = new DonationEngine()
         const donation: Donation = await donationEngine.createDonationManually(payload)
+        const notificationEngine = new NotificationEngine()
+        await notificationEngine.emailDonationReceipt(donation)
         return res.status(200).send({ data: donation })
     } catch (error) {
+        console.error(error)
         Sentry.captureException(error)
         return res.status(500).send({
             error: "Sorry, something went wrong creating this donation",
